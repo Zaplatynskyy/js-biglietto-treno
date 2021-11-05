b2fda6f524b5f1535afde07bcf03e22c1476d812
Igor <zaplatynskyy@gmail.com>
2021-11-05 16:12:47 +0100

    add html per stampa risultato js

--- js/script.js
+++ js/script.js
@@ -9,17 +9,23 @@ if (isNaN(chilometri) || isNaN(eta)) {
 let prezzoBiglietto = chilometri * 0.21;
 prezzoBiglietto = parseFloat(prezzoBiglietto.toFixed(2));
 
-console.log(prezzoBiglietto, 'euro Intero costo del biglietto');
-
+document.getElementById('chilometri').innerHTML = chilometri + 'Km';
+document.getElementById('eta').innerHTML = eta + 'anni';
 
 if (eta < 18){
     prezzoBiglietto = prezzoBiglietto - (prezzoBiglietto * 20 / 100);
     prezzoBiglietto = parseFloat(prezzoBiglietto.toFixed(2));
 
-    console.log(prezzoBiglietto, 'euro Scontato al 20% per i minorenni');
+    document.getElementById('prezzo_biglietto').innerHTML = prezzoBiglietto + 'euro';
+    document.getElementById('nota').innerHTML = '*prezzo scontato al 20% per gli under 18';
+
 } else if (eta > 65) {
     prezzoBiglietto = prezzoBiglietto - (prezzoBiglietto * 40 / 100);
     prezzoBiglietto = parseFloat(prezzoBiglietto.toFixed(2));
 
-    console.log(prezzoBiglietto, 'euro Scontato al 40% per gli over 65');
-} 
+    document.getElementById('prezzo_biglietto').innerHTML = prezzoBiglietto + 'euro';
+    document.getElementById('nota').innerHTML = '*prezzo scontato al 20% per gli over 65';
+    
+} else {
+    document.getElementById('prezzo_biglietto').innerHTML = prezzoBiglietto + 'euro';
+}
