3d95171928d4bb756b15d6676aa7ede715cc05d4
Igor <zaplatynskyy@gmail.com>
2021-11-06 15:55:14 +0100

    collegamento funzionalità html/js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,31 @@
-// let chilometri = parseFloat(prompt('Inserisci i chilometri da percorrere'));
-// let eta = parseInt(prompt('Inserisci la tua età'));
+let chilometri = parseFloat(prompt('Inserisci i chilometri da percorrere'));
+let eta = parseInt(prompt('Inserisci la tua età'));
 
-// if (isNaN(chilometri) || isNaN(eta)) {
-//     alert('Attenzione, hai inserito dei valori non validi. Ricarica e reinserisci i tuoi dati.');
-//     console.log(bloccoProgramma);
-// }
+if (isNaN(chilometri) || isNaN(eta)) {
+    alert('Attenzione, hai inserito dei valori non validi. Ricarica e reinserisci i tuoi dati.');
+    console.log(bloccoProgramma);
+}
 
-// let prezzoBiglietto = chilometri * 0.21;
-// prezzoBiglietto = parseFloat(prezzoBiglietto.toFixed(2));
+let prezzoBiglietto = chilometri * 0.21;
+prezzoBiglietto = parseFloat(prezzoBiglietto.toFixed(2));
 
-// document.getElementById('chilometri').innerHTML = chilometri + 'Km';
+document.getElementById('chilometri').innerHTML = chilometri + 'Km';
 // document.getElementById('eta').innerHTML = eta + 'anni';
 
-// if (eta < 18){
-//     prezzoBiglietto = prezzoBiglietto - (prezzoBiglietto * 20 / 100);
-//     prezzoBiglietto = parseFloat(prezzoBiglietto.toFixed(2));
+if (eta < 18){
+    prezzoBiglietto = prezzoBiglietto - (prezzoBiglietto * 20 / 100);
+    prezzoBiglietto = parseFloat(prezzoBiglietto.toFixed(2));
 
-//     document.getElementById('prezzo_biglietto').innerHTML = prezzoBiglietto + 'euro';
-//     document.getElementById('nota').innerHTML = '*prezzo scontato al 20% per gli under 18';
+    document.getElementById('prezzo_biglietto').innerHTML = prezzoBiglietto + 'euro';
+    // document.getElementById('nota').innerHTML = '*prezzo scontato al 20% per gli under 18';
 
-// } else if (eta > 65) {
-//     prezzoBiglietto = prezzoBiglietto - (prezzoBiglietto * 40 / 100);
-//     prezzoBiglietto = parseFloat(prezzoBiglietto.toFixed(2));
+} else if (eta > 65) {
+    prezzoBiglietto = prezzoBiglietto - (prezzoBiglietto * 40 / 100);
+    prezzoBiglietto = parseFloat(prezzoBiglietto.toFixed(2));
 
-//     document.getElementById('prezzo_biglietto').innerHTML = prezzoBiglietto + 'euro';
-//     document.getElementById('nota').innerHTML = '*prezzo scontato al 20% per gli over 65';
+    document.getElementById('prezzo_biglietto').innerHTML = prezzoBiglietto + 'euro';
+    // document.getElementById('nota').innerHTML = '*prezzo scontato al 20% per gli over 65';
     
-// } else {
-//     document.getElementById('prezzo_biglietto').innerHTML = prezzoBiglietto + 'euro';
-// }
+} else {
+    document.getElementById('prezzo_biglietto').innerHTML = prezzoBiglietto + 'euro';
+}
