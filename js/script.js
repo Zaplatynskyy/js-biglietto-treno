705cd46efea8b8431ece8ed361efe83f15c86563
Igor <zaplatynskyy@gmail.com>
2021-11-06 16:14:34 +0100

    add funzionalità js

--- js/script.js
+++ js/script.js
@@ -10,22 +10,34 @@ let prezzoBiglietto = chilometri * 0.21;
 prezzoBiglietto = parseFloat(prezzoBiglietto.toFixed(2));
 
 document.getElementById('chilometri').innerHTML = chilometri + 'Km';
-// document.getElementById('eta').innerHTML = eta + 'anni';
+document.getElementById('prezzo_iniziale').innerHTML = prezzoBiglietto + ' €';
+document.getElementById('passeggero').innerHTML = '1 Adulto';
+document.getElementById("carrozza").innerHTML =
+Math.floor(Math.random() * 8) + 1;
+document.getElementById("posto").innerHTML =
+Math.floor(Math.random() * 28) + 1;
+
 
 if (eta < 18){
     prezzoBiglietto = prezzoBiglietto - (prezzoBiglietto * 20 / 100);
     prezzoBiglietto = parseFloat(prezzoBiglietto.toFixed(2));
 
-    document.getElementById('prezzo_biglietto').innerHTML = prezzoBiglietto + 'euro';
-    // document.getElementById('nota').innerHTML = '*prezzo scontato al 20% per gli under 18';
+    document.getElementById('prezzo_totale').innerHTML = prezzoBiglietto + ' €';
+    document.getElementById('nota').innerHTML = 'under 18';
+    document.getElementById('passeggero').innerHTML = '1 Ragazzo/a';
+    document.getElementById('sconto').innerHTML = 'sconto ' + (prezzoBiglietto * 20 / 100).toFixed(2) + ' €';
 
 } else if (eta > 65) {
     prezzoBiglietto = prezzoBiglietto - (prezzoBiglietto * 40 / 100);
     prezzoBiglietto = parseFloat(prezzoBiglietto.toFixed(2));
 
-    document.getElementById('prezzo_biglietto').innerHTML = prezzoBiglietto + 'euro';
-    // document.getElementById('nota').innerHTML = '*prezzo scontato al 20% per gli over 65';
+    document.getElementById('prezzo_totale').innerHTML = prezzoBiglietto + ' €';
+    document.getElementById('nota').innerHTML = 'over 65';
+    document.getElementById('passeggero').innerHTML = '1 Anziano';
+    document.getElementById('sconto').innerHTML = 'sconto ' + (prezzoBiglietto * 40 / 100).toFixed(2) + ' €';
     
 } else {
-    document.getElementById('prezzo_biglietto').innerHTML = prezzoBiglietto + 'euro';
+    document.getElementById('prezzo_totale').innerHTML = prezzoBiglietto + ' €';
+    document.getElementById('nota').innerHTML = 'Tariffa intera';
+    document.getElementById('sconto').innerHTML = 'non scontato';
 }
